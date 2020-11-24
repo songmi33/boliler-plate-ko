b6a150b69b0c218b2cc46f90a0c28930fc58ff08
songmi33 <thdal1232@gmail.com>
2020-11-24 16:56:10 +0900

    비밀번호 암호화

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ app.post("/register", (req, res) => {
 
   const user = new User(req.body)
 
+  
   user.save((err, userInfo)=>{
     if(err) return res.json({success:false, err})
     return res.status(200).json({    // status(200)은 성공했다는 의미
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,25 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.pre("save", function( next ){
+    
+    var user = this;
+    // 패스워드가 변환될때만 암호화
+    if(user.isModified("password")){
+        // 비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })   
+    } else{
+        // 비밀 번호를 바꾸지 않는다면 바로 넘어갈 수 있도록 함
+        next()
+    }   
+})
 const User = mongoose.model("User", userSchema)
 
 module.exports = { User }    // 다른 파일에서도 사용할 수 있도록 export 해줌
