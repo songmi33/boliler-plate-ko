e74bd738cad4acd3013dfa6e4559f7c6cd4f7eb3
songmi33 <thdal1232@gmail.com>
2020-11-24 16:23:56 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
 
+const config = require("./config/key");
+
+// application/x-www-form=urlencoded를 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.urlencoded({extended:true}));
+
+// application/json을 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://songmi:a102092632@bolierplate.jmgkv.mongodb.net/<dbname>?retryWrites=true&w=majority",{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }).then(()=>console.log("MongoDB Connected..."))
   .catch(err => console.log(err))
 
+
+
 app.get('/', (req, res) => {
-  res.send('안녕하세요!')
+  res.send('새해복 많이 받으세요!')
+})
+
+
+app.post("/register", (req, res) => {
+  // 회원가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo)=>{
+    if(err) return res.json({success:false, err})
+    return res.status(200).json({    // status(200)은 성공했다는 의미
+      success:true
+    })
+  })
 })
 
 app.listen(port, () => {
